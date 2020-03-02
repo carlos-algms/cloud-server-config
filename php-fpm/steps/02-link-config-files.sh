cd860bfaf861708a95accca602c67b1196629d8c
Carlos Gomes <carlos.algms@gmail.com>
2020-03-02 18:02:08 +0100

    Created custom access log for PHP-fpm pool

--- php-fpm/steps/02-link-config-files.sh
+++ php-fpm/steps/02-link-config-files.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 echo "ℹ️ Linking INI files"
+ORIGIN="${SCRIPT_DIR}/snippets/conf.d"
 FPM_CONF_D="/etc/php/${PHP_VER}/fpm/conf.d"
 CLI_CONF_D="/etc/php/${PHP_VER}/cli/conf.d"
-ORIGIN="${SCRIPT_DIR}/snippets/conf.d"
 
 ln -sf ${ORIGIN}/* "${FPM_CONF_D}"
 ln -sf ${ORIGIN}/* "${CLI_CONF_D}"
+
+
+POOL_ORIGIN="${SCRIPT_DIR}/snippets/conf.d"
+POOL_CONF_D="/etc/php/${PHP_VER}/fpm/pool.d"
+
+ln -sf ${POOL_ORIGIN}/* "${POOL_CONF_D}"
